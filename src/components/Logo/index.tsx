251e26b5a26cd831a332af7de114ae21dd5aef0a
Arian Baishya <arian.baishya2004@gmail.com>
2024-10-14 15:34:15 -0500

    remove commented out CSS gradient
    i think i'm just going to give up on this

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -9,7 +9,6 @@ export default function Logo() {
             <img src={LogoImageUrl} alt="CRC Logo" style={{
                 height: logoSize,
                 width: logoSize,
-                // backgroundImage: "linear-gradient(-45deg, rgb(189, 52, 254) 50%, rgb(255, 54, 112) 50%)"
             }}/>
         </div>
     )
