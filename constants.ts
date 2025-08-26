97d5fd451a4ae4d528e5fa7d4f70f395399a7539
Joshua Purushothaman <joshuathooyavan1@gmail.com>
2025-08-26 07:49:43 -0500

    Update constants.ts to fix build errors with Sidebar
    Well i guess i can keep the "learning typescript" on my resume?

--- constants.ts
+++ constants.ts
@@ -7,7 +7,8 @@ type ProjectDocs = {
 const LIST_OF_PROJECT_DOCS: ProjectDocs[] = [
     {label: "Start Here", dirName: "start-here", sidebarID: "startHereSidebar"},
     {label: "EnMo", dirName: "enmo", sidebarID: "enmoSidebar"},
-    {label: "STORM", dirName: "storm", sidebarID: "stormSidebar"},
+    {label: "STORM 2025 (Archive)", dirName: "storm-2025", sidebarID: "storm2025Sidebar"},
+    {label: "STORM 2026", dirName: "storm-2026", sidebarID: "storm2026Sidebar"},
 ] as const;
 
 export default LIST_OF_PROJECT_DOCS
